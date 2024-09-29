07d0bf0f5a0a3978ea94241042aa44bf18a4cb92
HACK-WU <2931580128@qq.com>
2024-09-29 20:20:36 +0800

    修改Cacheresource实现 # Reviewed, transaction id: 19752

--- bkmonitor/core/drf_resource/contrib/cache.py
+++ bkmonitor/core/drf_resource/contrib/cache.py
@@ -9,7 +9,6 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-
 import abc
 
 import six
@@ -31,6 +30,8 @@ class CacheResource(six.with_metaclass(abc.ABCMeta, Resource)):
     cache_user_related = None
     # 是否使用压缩
     cache_compress = True
+    # 缓存对象
+    cache = using_cache
 
     def __init__(self, *args, **kwargs):
         # 若cache_type为None则视为关闭缓存功能
@@ -55,17 +56,13 @@ class CacheResource(six.with_metaclass(abc.ABCMeta, Resource)):
         将原有的request方法替换为支持缓存的request方法
         """
 
-        def func_key_generator(resource):
-            key = "{}.{}".format(resource.__self__.__class__.__module__, resource.__self__.__class__.__name__)
-            return key
-
-        self.request = using_cache(
+        self.request = self.cache(
             cache_type=self.cache_type,
             backend_cache_type=self.backend_cache_type,
             user_related=self.cache_user_related,
             compress=self.cache_compress,
             is_cache_func=self.cache_write_trigger,
-            func_key_generator=func_key_generator,
+            func_key_generator=self.func_key_generator,
         )(self.request)
 
     def cache_write_trigger(self, res):
@@ -73,3 +70,8 @@ class CacheResource(six.with_metaclass(abc.ABCMeta, Resource)):
         缓存写入触发条件
         """
         return True
+
+    @staticmethod
+    def func_key_generator(resource):
+        key = "{}.{}".format(resource.__self__.__class__.__module__, resource.__self__.__class__.__name__)
+        return key
