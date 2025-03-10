43c311fda19d80e1760bcdb9455b870efc406df0
chenguo <chenguo367@163.com>
2025-03-10 16:57:54 +0800

    minor: check_plugin_status命令提示在api环境下执行优化

--- bkmonitor/bkmonitor/management/commands/check_plugin_status.py
+++ bkmonitor/bkmonitor/management/commands/check_plugin_status.py
@@ -15,8 +15,6 @@ from typing import Dict, Tuple
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from monitor_web.commons.data_access import PluginDataAccessor
-from monitor_web.models import CollectorPluginMeta
 
 from metadata.models import (
     DataSource,
@@ -26,6 +24,8 @@ from metadata.models import (
     TimeSeriesMetric,
 )
 from metadata.utils import consul_tools
+from monitor_web.commons.data_access import PluginDataAccessor
+from monitor_web.models import CollectorPluginMeta
 
 logger = logging.getLogger("metadata")
 
@@ -87,10 +87,10 @@ class Command(BaseCommand):
         return result, correct_flag
 
     def handle(self, *args, **options):
-        if settings.ROLE != "api":
-            print("try with: ./bin/api_manage.sh change_plugin_biz --arguments")
-            return
         plugin_id = options["plugin_id"]
+        if settings.ROLE != "api":
+            print(f"try with: ./bin/api_manage.sh check_plugin_status --plugin_id={plugin_id}")
+            return
         # 获取 plugin 信息
         try:
             plugin = CollectorPluginMeta.objects.get(plugin_id=plugin_id)
