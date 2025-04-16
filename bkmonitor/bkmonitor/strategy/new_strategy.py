1eb22cb5f5597e6dc78b7f4830ec3137d1671ead
chenguo <chenguo367@163.com>
2025-04-16 17:48:20 +0800

    minor: 计算平台时序数据配置高级过滤条件优化维度补充

--- bkmonitor/bkmonitor/strategy/new_strategy.py
+++ bkmonitor/bkmonitor/strategy/new_strategy.py
@@ -1407,6 +1407,9 @@ class QueryConfig(AbstractConfig):
             # 识别高级计算方法并收集维度字段
             if condition["method"] in data_source.ADVANCE_CONDITION_METHOD:
                 has_advance_method = True
+            # 数值型字段，不需要进行聚合分组
+            if condition["method"] in ["gt", "lt", "gte", "lte"]:
+                continue
             dimensions.add(condition["key"])
 
         if has_advance_method:
