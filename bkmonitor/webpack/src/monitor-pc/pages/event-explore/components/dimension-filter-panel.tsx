7a75079ab5d705ed55ce3ddfdb4421480d3a2352
闪烁 <57348011+shan-shuo@users.noreply.github.com>
2025-04-22 15:40:02 +0800

    feat: 完成事件检索字段分析图表功能 (#6045)

--- bkmonitor/webpack/src/monitor-pc/pages/event-explore/components/dimension-filter-panel.tsx
+++ bkmonitor/webpack/src/monitor-pc/pages/event-explore/components/dimension-filter-panel.tsx
@@ -140,7 +140,7 @@ export default class DimensionFilterPanel extends tsc<DimensionFilterPanelProps,
     this.popoverInstance = this.$bkPopover(e.currentTarget, {
       content: this.statisticsListRef.$refs.dimensionPopover,
       placement: 'right',
-      width: 400,
+      width: 405,
       distance: -5,
       boundary: 'viewport',
       trigger: 'manul',
@@ -338,8 +338,10 @@ export default class DimensionFilterPanel extends tsc<DimensionFilterPanelProps,
 
         <StatisticsList
           ref='statisticsList'
+          fieldType={this.slideField?.type}
           isDimensions={this.slideField?.is_dimensions}
           isShow={this.showStatisticsPopover}
+          isShowChart={true}
           popoverInstance={this.popoverInstance}
           selectField={this.slideField?.name}
           source={this.source}
