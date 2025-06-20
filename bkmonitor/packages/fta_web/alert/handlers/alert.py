29dfc6d284f7eaf8a7bafd89d0e007187bade343
HACK-WU <2931580128@qq.com>
2025-06-20 09:11:36 +0000

    feat: 【告警中心】新增基于处理套餐的检索 #1010158081124785004

--- bkmonitor/packages/fta_web/alert/handlers/alert.py
+++ bkmonitor/packages/fta_web/alert/handlers/alert.py
@@ -23,6 +23,7 @@ from luqum.tree import FieldGroup, OrOperation, Phrase, SearchField, Word
 
 from bkmonitor.documents import ActionInstanceDocument, AlertDocument, AlertLog
 from bkmonitor.models import ActionInstance, ConvergeRelation, MetricListCache, Shield
+from bkmonitor.models.fta.action import ActionConfig
 from bkmonitor.strategy.new_strategy import get_metric_id
 from bkmonitor.utils.ip import exploded_ip
 from bkmonitor.utils.request import get_request_tenant_id
@@ -135,6 +136,7 @@ class AlertQueryTransformer(BaseQueryTransformer):
         QueryField("ack_duration", _lazy("确认时间")),
         QueryField("data_type", _lazy("数据类型"), es_field="event.data_type"),
         QueryField("action_id", _lazy("处理记录ID"), es_field="id"),
+        QueryField("action_name", _lazy("处理套餐名称"), es_field="id"),
         QueryField("converge_id", _lazy("收敛记录ID"), es_field="id"),
         QueryField("event_id", _lazy("事件ID"), es_field="event.event_id", is_char=True),
         QueryField("plugin_id", _lazy("告警来源"), es_field="event.plugin_id", is_char=True),
@@ -180,6 +182,19 @@ class AlertQueryTransformer(BaseQueryTransformer):
                     # 尝试将匹配翻译值，并转换回原值
                     if display == node.value:
                         node.value = str(value)
+
+            # 用于支持对处理套餐名称的查询
+            elif search_field_name == "id" and context.get("search_field_origin_name") in [
+                "action_name",
+                _("处理套餐名称"),
+            ]:
+                action_config_ids = ActionConfig.objects.filter(name=node.value).values_list("id", flat=True)
+                alert_id_ids = ActionInstance.objects.filter(action_config_id__in=action_config_ids).values_list(
+                    "alerts", flat=True
+                )
+                alert_ids = list(chain.from_iterable(alert_id_ids))
+                node = FieldGroup(OrOperation(*[Word(str(alert_id)) for alert_id in alert_ids or [0]]))
+                context = {"ignore_search_field": True, "ignore_word": True}
 
             # 特殊处理动作ID字段：从动作实例提取关联告警ID
             elif search_field_name == "id" and context.get("search_field_origin_name") in [
@@ -1175,6 +1190,7 @@ class AlertQueryHandler(BaseBizQueryHandler):
         # 系统保留字段清理：
         # 移除内部流转使用的action_id字段
         cleaned_data.pop("action_id", None)
+        cleaned_data.pop("action_name", None)
 
         # 业务展示字段补充：
         # 添加格式化展示字段和计算字段
