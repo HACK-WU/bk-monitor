818c105c7cda89bf2700099a15f57f9e9fb56db2
HACK-WU <2931580128@qq.com>
2025-08-04 04:03:07 +0000

    feat: 删除不必要的文件

--- bklog/console/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import re_path  # noqa
-
-from console import views  # noqa
-
-urlpatterns = (re_path(r"^accounts/logout", views.user_exit),)
--- bklog/log_adapter/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include
-from django.urls import re_path
-
-app_name = "log_adapter"
-
-urlpatterns = [
-    re_path(r"^", include("log_adapter.home.urls")),
-]
